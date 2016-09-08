2710459ceb2e7145df6b8fa6fbcd97eff29b755b
ranglan <lanziwen@outlook.com>
2016-09-08 20:09:29 +0800

    feat: add canvas

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ import Cycle from '@cycle/xstream-run'
 import {makeDOMDriver} from '@cycle/dom'
 import {makeRouterDriver, supportsHistory} from 'cyclic-router'
 import {createHistory, createHashHistory} from 'history'
+import {makeCanvasDriver} from './driver/canvas-driver'
+// import {makePixiDriver} from './driver/pixi-driver'
 
 // Local imports
 import main from 'page/main'
@@ -15,7 +17,9 @@ const history = supportsHistory()
 const drivers =
   {
     DOM: makeDOMDriver('#app', {transposition: false}),
-    router: makeRouterDriver(...history)
+    router: makeRouterDriver(...history),
+    Canvas: makeCanvasDriver('#canvas', {width: 800, height: 600})
+    // pixi: makePixiDriver(document.getElementById('game'), 800, 600)
   }
 
 Cycle.run(main, drivers)
--- src/page/Login/index.js
+++ src/page/Login/index.js
@@ -1,12 +1,42 @@
 import xs from 'xstream'
 import isolate from '@cycle/isolate'
 import {div, h1} from '@cycle/dom'
+import {rect, text} from '../../driver/canvas-driver'
+
+function renderGameOverSplash () {
+  const props = {
+    x: 400,
+    y: 300,
+    font: '72pt Arial',
+    textAlign: 'center',
+    value: 'Game Over'
+  }
+
+  const subTextProps = {
+    x: 0,
+    y: 50,
+    font: '25pt Arial',
+    textAlign: 'center',
+    fillStyle: 'red',
+    value: 'Press Space to play again'
+  }
+
+  return (
+    text(props, [
+      text({value: 'Press Space to play again', ...subTextProps})
+    ])
+  )
+}
 
 function Login (sources) {
   return {
     DOM: xs.of(
       div({}, [
         h1('.login', 'Login page')
+      ])),
+    Canvas: xs.of(
+      rect({draw: [{fill: 'skyblue'}]}, [
+        renderGameOverSplash()
       ])
     )
   }
