1e860c4c6a6f0b0a8e562ebe66348de8e1fe99ee
ranglan <lanziwen@outlook.com>
2016-09-13 15:05:02 +0800

    fix: house

--- src/page/Login/index.js
+++ src/page/Login/index.js
@@ -2,14 +2,36 @@ import xs from 'xstream'
 import isolate from '@cycle/isolate'
 import tween from 'xstream/extra/tween'
 import {div, button, img} from '@cycle/dom'
-// import {rect, text} from '../../driver/canvas-driver' // text
-// import fromEvent from 'xstream/extra/fromEvent'
 
-// import pairwise from 'xstream/extra/pairwise'
-// import {difference, sortBy} from 'lodash'// intersection
+function locationModal (data) {
+  const locationModalStyle = {
+    left: `${Math.round(data.x)}px`,
+    top: `${Math.round(data.y)}px`,
+    width: `${Math.round(data.width)}px`,
+    height: `${Math.round(data.height)}px`,
+    position: 'absolute',
+    alignItems: 'center',
+    display: 'flex',
+    flexDirection: 'column',
+    backgroundColor: 'blue',
+    paddingTop: '70px',
+    border: 'none'
+  }
+
+  if (data.value === 'house') {
+    return div({style: locationModalStyle},
+      [img({props: {src: 'images/bg-shop.png'}})]
+    )
+  } else if (data.value === 'market') {
+    return div({style: locationModalStyle},
+      [img({props: {src: 'images/bg-shop.png'}})]
+    )
+  } else {
+    return null
+  }
+}
 
 function loginModal (value) {
-  console.log(value)
   const loginModalStyle = {
     left: `${Math.round(value.x)}px`,
     top: `${Math.round(value.y)}px`,
@@ -42,6 +64,7 @@ function main (sources) {
   const openModal$ = sources.DOM.select('.login').events('click').mapTo()
   const closeModal$ = sources.DOM.select('.cancle').events('click').mapTo()
 
+  // ji
   let tweenY$ = tween({
     from: 0, to: 300, duration: 500, ease: tween.power3.easeIn
   })
@@ -101,43 +124,46 @@ function main (sources) {
 
   const login$ = xs.merge(open$, close$).startWith(startState)
 
+  const startState1 = {
+    value: 'null',
+    x: 0,
+    y: 0,
+    width: 0,
+    height: 0
+  }
+
   const action2$ = xs.merge(houseModal$, marketModal$).fold((acc, x) => {
     if (acc === x) return 'null'
     else return x
-  }, 'null')
-
-  // const action1$ = xs.merge(openModal$, cancleModal$).fold((acc, x) => {
-  //   if (x === 1) {
-  //     return ({
-  //       value: 1,
-  //       x: 318,
-  //       y: 195,
-  //       width: 500,
-  //       height: 300
-  //     })
-  //   } else {
-  //     return {
-  //       value: 0,
-  //       x: 980,
-  //       y: 1204,
-  //       width: 0,
-  //       height: 0
-  //     }
-  //   }
-  // }, startState)
+  }, 'null').map(location => {
+    if (location === 'null') {
+      return xs.combine(tweenX$, tweenY$, tweenW$, tweenH$).map(data => ({
+        value: location,
+        x: data[0],
+        y: data[1],
+        width: data[2],
+        height: data[3]
+      }))
+    } else {
+      return xs.combine(tween1X$, tween1Y$, tween1W$, tween1H$).map(data => ({
+        value: location,
+        x: data[0],
+        y: data[1],
+        width: data[2],
+        height: data[3]
+      }))
+    }
+  }).flatten().startWith(startState1)
 
   const action$ = xs.combine(login$, action2$)
-
   const loginStyle = {fontSize: '36px', padding: '0', listStyle: 'none', display: 'flex', justifyContent: 'flex-end', width: '100%'}
   const btnStyle = {marginTop: '15px', marginRight: '50px', position: 'absolute'}
-
   const count$ = action$
   const vtree$ = count$.map(data =>
       div([
         div({style: loginStyle},
           [button('.login', {style: btnStyle}, 'Login')]
         ),
-        // data[0].value > 0 ? loginModal(data[0]) : null,
         loginModal(data[0]),
         div(
           [
@@ -146,8 +172,9 @@ function main (sources) {
           ]
         ),
         div('.backgounddiv',
-          [img({props: {src: 'images/shop.png'}})]
-        )
+          [img({props: {src: 'images/null.png'}})]
+        ),
+        locationModal(data[1])
       ])
     )
   return {
